ab12364fbdfdaa7e7ad144eb2869077060b69fdc
Nathan Onn <nathan.incsub@gmail.com>
2018-06-30 13:50:55 +0800

    updates

--- src/container/TaskView/index.js
+++ src/container/TaskView/index.js
@@ -4,8 +4,16 @@ import './style.css'
 class TaskView extends Component {
   render() {
     return (
-      <div className="sample">
-        TaskView
+      <div className="task-view">
+        <h4 className="task-ad-company">AirAsia Phuket Billboard ( By BillBoard Media)</h4>
+        <div className="task-ad-location">123, jalan satu, bandar satu</div>
+        <div className="task-ad-image">
+          <img src="/assets/images/ad_board.jpg" />
+        </div>
+        <div className="task-ad-meta">
+          <div>Start Date: 1st Jan 2018</div>
+          <div>End Date: 31st Dec 2018</div>
+        </div>
       </div>
     )
   }
